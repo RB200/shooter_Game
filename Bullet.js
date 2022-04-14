da9379b8130425138720981d026bb20796a463d4
RB200 <66707944+RB200@users.noreply.github.com>
2022-04-13 18:47:25 -0700

    Add files via upload

---
+++ Bullet.js
@@ -0,0 +1,38 @@
+AFRAME.registerComponent('bullets',{
+    init: function(){
+        this.shootBullet()
+    },
+
+    shootBullet: function(){
+        window.addEventListener('keydown', (e) =>{
+            if(e.key === 'z'){
+                var bullet = document.createElement('a-entity')
+                bullet.setAttribute('geometry',{
+                    primitive:'sphere',
+                    radius:0.1
+                })
+                bullet.setAttribute('material','color','black')
+
+                var cam = document.querySelector('#camera')
+                position = cam.getAttribute('position')
+                
+                bullet.setAttribute('position',{
+                    x:position.x,
+                    y:position.y,
+                    z:position.z,
+                })
+                
+                var camera = document.querySelector('#camera').object3d
+                var direction = new THREE.Vector3()
+                camera.getWorldDirection(direction)
+                console.log(direction)
+                
+
+                bullet.setAttribute('velocity',direction.multiplyScaler(-10))
+
+                var scene = document.querySelector('#scene')
+                scene.appendChild(bullet)
+            }
+        })
+    }
+})
